b43663c6fdc9d876b31da954bf3cb005c30f2ea2
Lam Pham <phamla@dickinson.edu>
2025-11-13 15:46:16 -0500

    install tailwind have the sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Sidebar = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div className="absolute top-0 bottom-0 left-0 w-[50px] bg-black flex flex-col justify-center items-center gap-1">
+        {" "}
+        <div onClick={() => navigate("/")}>Home</div>
+        <div onClick={() => navigate("/crewmate")}>CrewMateDetails</div>
+        <div onClick={() => navigate("/createCrewmate")}>CreateCrewMate</div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
