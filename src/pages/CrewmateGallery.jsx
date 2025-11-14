2c958eaaecddf8a3bd4b1cb3dd06ef43f2411120
Lam Pham <phamla@dickinson.edu>
2025-11-13 21:19:54 -0500

    Refactor CrewmateGallery to fetch crewmates from Supabase and remove local data file

--- src/pages/CrewmateGallery.jsx
+++ src/pages/CrewmateGallery.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
-import { crewmates as initialCrewmates } from "../data/data";
+import React, { useState, useEffect } from "react";
 import CrewMateCard from "../components/CrewMateCard";
+import { supabase } from "../supabaseClient";
 
 const CrewmateGallery = () => {
-  const [crewmates, setCrewmates] = useState(initialCrewmates);
+  const [crewmates, setCrewmates] = useState([]);
   const [editingId, setEditingId] = useState(null);
+  const fetchCrewmates = async () => {
+    const { data, error } = await supabase.from("Crewmates").select("*");
+    if (error) {
+      console.error(error);
+    } else {
+      setCrewmates(data);
+    }
+  };
 
   const handleEditToggle = (id) => {
     setEditingId(editingId === id ? null : id);
@@ -13,24 +21,28 @@ const CrewmateGallery = () => {
     setCrewmates((prev) =>
       prev.map((mate) => (mate.id === updatedMate.id ? updatedMate : mate))
     );
-    setEditingId(null); // close editing
+    setEditingId(null);
   };
+  useEffect(() => {
+    fetchCrewmates();
+  }, []);
 
   return (
     <div>
-      {crewmates.map((crewmate) => (
-        <CrewMateCard
-          key={crewmate.id}
-          id={crewmate.id}
-          src={crewmate.src}
-          name={crewmate.name}
-          speed={crewmate.speed}
-          color={crewmate.color}
-          onEditToggle={() => handleEditToggle(crewmate.id)}
-          isEditing={editingId === crewmate.id}
-          onSave={handleSave}
-        />
-      ))}
+      {crewmates &&
+        crewmates.map((crewmate) => (
+          <CrewMateCard
+            key={crewmate.id}
+            id={crewmate.id}
+            src={crewmate.src}
+            name={crewmate.name}
+            speed={crewmate.speed}
+            color={crewmate.color}
+            onEditToggle={() => handleEditToggle(crewmate.id)}
+            isEditing={editingId === crewmate.id}
+            onSave={handleSave}
+          />
+        ))}
     </div>
   );
 };
